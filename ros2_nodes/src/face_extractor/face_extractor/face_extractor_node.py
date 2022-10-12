307f3ac54567abba2e6ffc9288ed38a35a3e845f
Shintaro Sakoda <sakoda@keio.jp>
2022-10-12 10:47:25 +0000

    face_region_msg追加

--- ros2_nodes/src/face_extractor/face_extractor/face_extractor_node.py
+++ ros2_nodes/src/face_extractor/face_extractor/face_extractor_node.py
@@ -4,6 +4,9 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 import sys
+import numpy as np
+
+from face_region_msg.msg import FaceRegion
 
 # sys.path.append('../../../../src/feature_extractor/')  # nopep8
 sys.path.append('../src/feature_extractor/')  # nopep8
@@ -14,6 +17,7 @@ class MySubscriberNode(Node):
     def __init__(self):
         super().__init__("face_extractor_node")
         self.subscription = self.create_subscription(Image, "image_publisher", self.on_subscribe, 10)
+        self.publisher = self.create_publisher(FaceRegion, "face_region", 10)
         self.face_extractor_ = FaceExtractor(True)
 
     def on_subscribe(self, msg):
@@ -21,6 +25,11 @@ class MySubscriberNode(Node):
         img = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         result = self.face_extractor_.execute(img)
         self.get_logger().info(f"Subscribe image")
+        msg = FaceRegion()
+        msg.num = len(result)
+        msg.region_points = list(np.array(result).flatten())
+        self.publisher.publish(msg)
+        self.get_logger().info(f"Publish msg")
 
 
 def main(args=None):
