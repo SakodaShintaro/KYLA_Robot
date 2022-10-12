5eb0c148c4c5386f566099612274f5c43983b5b5
Shintaro Sakoda <sakoda@keio.jp>
2022-10-12 10:05:51 +0000

    顔画像位置の出力を追加

--- ros2_nodes/src/face_extractor/face_extractor/face_extractor_node.py
+++ ros2_nodes/src/face_extractor/face_extractor/face_extractor_node.py
@@ -3,17 +3,23 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
+import sys
+
+# sys.path.append('../../../../src/feature_extractor/')  # nopep8
+sys.path.append('../src/feature_extractor/')  # nopep8
+from face_roi_extractor import FaceExtractor  # nopep8
 
 
 class MySubscriberNode(Node):
     def __init__(self):
         super().__init__("face_extractor_node")
         self.subscription = self.create_subscription(Image, "image_publisher", self.on_subscribe, 10)
+        self.face_extractor_ = FaceExtractor(True)
 
     def on_subscribe(self, msg):
         bridge = CvBridge()
         img = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        cv2.imwrite("qwe.png", img)
+        result = self.face_extractor_.execute(img)
         self.get_logger().info(f"Subscribe image")
 
 
