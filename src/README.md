7526e0bca287c8fbca7d02ee6da786e0aedec204
Shintaro Sakoda <sakoda@keio.jp>
2022-12-07 11:05:50 +0000

    pipでインストール可能なように追加

---
+++ src/README.md
@@ -0,0 +1,4 @@
+## feature_extractorを階層位置によらず使えるようにする
+```bash
+pip3 install -e .
+```
---
+++ src/feature_extractor/__init__.py
@@ -0,0 +1,3 @@
+from .face_roi_extractor import FaceRoiExtractor
+from .face_feature_extractor import FaceFeatureExtractor
+__all__ = ["FaceRoiExtractor", "FaceFeatureExtractor"]
