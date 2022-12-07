93d0448f19663c76c178e152a5534cfd1910f334
Shintaro Sakoda <sakoda@keio.jp>
2022-12-07 10:34:42 +0000

    読み込みをinitへ移動

--- src/feature_extractor/face_feature_extractor.py
+++ src/feature_extractor/face_feature_extractor.py
@@ -7,19 +7,16 @@ import numpy as np
 
 class FaceFeatureExtractor:
     def __init__(self) -> None:
-        pass
+        self.device_ = torch.device('cuda')
+        self.model_ = iresnet100(pretrained=False)
+        self.model_.load_state_dict(torch.load(
+            '../../assets/models/backbone.pth',
+            map_location=self.device_))
+        self.model_.eval()
+        self.model_.to(self.device_)
 
     def execute(self, image_path_list):
-        device = torch.device('cuda')
-        model = iresnet100(pretrained=False)
-
         # ref: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch#model-zoo
-        model.load_state_dict(torch.load(
-            '../../assets/models/backbone.pth',
-            map_location=device))
-
-        model.eval()
-        model.to(device)
         image_tensor_list = list()
         for image_path in image_path_list:
             image = Image.open(image_path)
@@ -31,8 +28,8 @@ class FaceFeatureExtractor:
             image_tensor_list.append(image_tensor)
 
         torch_cat_image_tensor = torch.cat(image_tensor_list, 0)
-        torch_cat_image_tensor = torch_cat_image_tensor.to(device)
-        feat_list = model(torch_cat_image_tensor)
+        torch_cat_image_tensor = torch_cat_image_tensor.to(self.device_)
+        feat_list = self.model_(torch_cat_image_tensor)
 
         ret_processed_feat_list = list()
         for i in range(len(feat_list)):
