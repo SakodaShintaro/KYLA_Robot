d99a4ce51d5f9efb2169c2f9bfe45998f4774fd5
Shintaro Sakoda <sakoda@keio.jp>
2023-01-04 10:37:15 +0000

    不要なコード削除

--- ros2_nodes/src/face_identifier/face_identifier/face_identifier_impl.py
+++ ros2_nodes/src/face_identifier/face_identifier/face_identifier_impl.py
@@ -22,9 +22,6 @@ class FaceIdentifierNode(Node):
         self.image_list_ = list()
         self.region_list_ = list()
 
-        self.list_of_feature_ = list()
-        self.next_id_ = 0
-
         # IResNet config ---
         self.device = torch.device('cuda')
         self.model = iresnet100(pretrained=False)
@@ -69,34 +66,14 @@ class FaceIdentifierNode(Node):
         self.get_logger().info(f"len(image_list) = {len(image_list)}")
 
         if len(image_list) == 0:
-            self.list_of_feature_.clear()
             return
 
         result = self.face_matcher_(image_list)
-        for sim, name in result:
-            self.get_logger().info(f"sim = {sim:.4f}, name = {name}")
-
-        feature = self.infer(image_list)
-        self.get_logger().info(f"len(feature) = {len(feature)}")
-
-        next_feature_list = list()
-
-        for i, feat in enumerate(feature):
-            max_value = -float("inf")
-            max_index = -1
-
-            curr_id = None
-
-            if max_index == -1:
-                curr_id = self.next_id_
-                self.next_id_ += 1
-            else:
-                curr_id = self.list_of_feature_[max_index]
-
-            next_feature_list.append((feat, curr_id))
+        for i in range(len(region_list)):
             lux, luy, rdx, rdy = region_list[i]
             sim = result[i][0]
             name = result[i][1] if sim > 0.3 else "unknown" 
+            self.get_logger().info(f"sim = {sim:.4f}, name = {name}")
             cv2.putText(curr_image, f"{name}({sim:.3f})",
                         org=(rdx, rdy),
                         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
@@ -104,8 +81,6 @@ class FaceIdentifierNode(Node):
                         color=(0, 255, 0),
                         thickness=2,
                         lineType=cv2.LINE_4)
-
-        self.list_of_feature_ = next_feature_list
 
         bridge = CvBridge()
         curr_image = cv2.rotate(curr_image, cv2.ROTATE_90_CLOCKWISE)
@@ -119,43 +94,6 @@ class FaceIdentifierNode(Node):
 
     def on_subscribe_region(self, msg):
         self.region_list_.append(msg)
-
-    def infer(self, img_list):
-        self.model.eval()
-        self.model.to(self.device)
-        image_tensor_list = list()
-
-        for img in img_list:
-            image = self.__cv2pil(img)
-            image = image.convert("RGB")
-            image_tensor = torchvision.transforms.functional.to_tensor(image)
-            image_tensor = torchvision.transforms.functional.resize(
-                size=(112, 112), img=image_tensor)
-            image_tensor = torch.unsqueeze(image_tensor, 0)
-            image_tensor_list.append(image_tensor)
-
-        torch_cat_image_tensor = torch.cat(image_tensor_list, 0)
-        torch_cat_image_tensor = torch_cat_image_tensor.to(self.device)
-        feat_list = self.model(torch_cat_image_tensor)
-
-        result = list()
-        for feat in feat_list:
-            processed_feat = [elem.item() for elem in feat]
-            ret_processed_feat = np.array(processed_feat)
-            result.append(ret_processed_feat)
-        return result
-
-    def __cv2pil(self, image):
-        ''' OpenCV型 -> PIL型 '''
-        new_image = image.copy()
-        if new_image.ndim == 2:  # モノクロ
-            pass
-        elif new_image.shape[2] == 3:  # カラー
-            new_image = new_image[:, :, ::-1]
-        elif new_image.shape[2] == 4:  # 透過
-            new_image = new_image[:, :, [2, 1, 0, 3]]
-        new_image = PImage.fromarray(new_image)
-        return new_image
 
 
 def main(args=None):
