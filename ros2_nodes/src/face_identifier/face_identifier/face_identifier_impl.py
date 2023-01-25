707bab5d5863f623df98d7797ea6d6fe4d3280cf
Shintaro Sakoda <sakoda@keio.jp>
2023-01-25 10:50:04 +0000

    温度測定部分を仮追加

--- ros2_nodes/src/face_identifier/face_identifier/face_identifier_impl.py
+++ ros2_nodes/src/face_identifier/face_identifier/face_identifier_impl.py
@@ -15,8 +15,10 @@ class FaceIdentifierNode(Node):
         self.timer = self.create_timer(0.1, self.on_tick)
         self.subscription = self.create_subscription(CompressedImage, "image_publisher", self.on_subscribe_image, 10)
         self.subscription = self.create_subscription(FaceRegion, "face_region", self.on_subscribe_region, 10)
+        self.subscription = self.create_subscription(CompressedImage, "thermography_publisher", self.on_subscribe_thermo, 10)
         self.image_list_ = list()
         self.region_list_ = list()
+        self.thermo_list_ = list()
         self.face_matcher_ = FaceMatcher('/home/ubuntu/KYLA_Robot/assets/database/FACE_FEATURES.db')
 
     def on_tick(self):
@@ -27,9 +29,14 @@ class FaceIdentifierNode(Node):
         # TODO: 計算量的に悪くないか確認する
         curr_image = self.image_list_.pop(0)
         curr_region = self.region_list_.pop(0)
+        curr_thermo = self.thermo_list_.pop(0)
 
         curr_region = np.array(curr_region.region_points)
         curr_region = curr_region.reshape((-1, 4))
+
+        thermo_list =  self.get_thermo_list(curr_region, curr_thermo)
+        for i in range(len(thermo_list)):
+            self.get_logger().info(f"t[{i}] = {thermo_list[i]}")
 
         self.get_logger().info(f"curr_image.shape = {curr_image.shape}")
 
@@ -80,6 +87,20 @@ class FaceIdentifierNode(Node):
         msg = bridge.cv2_to_imgmsg(curr_image, encoding="bgr8")
         self.publisher.publish(msg)
 
+    def get_thermo_list(self, curr_region, thermo_image):
+        result = list()
+        image_height, image_width = thermo_image.shape[0:2]
+        for rect in curr_region:
+            lux = int(rect[0] * image_width)
+            luy = int(rect[1] * image_height)
+            rdx = max(int(rect[2] * image_width), lux + 1)
+            rdy = max(int(rect[3] * image_height), luy + 1)
+            cut_image = thermo_image[luy:rdy, lux:rdx]
+            average = np.mean(cut_image)
+            self.get_logger().info(f"In get_thermo_list : lux = {lux}, luy = {luy}, rdx = {rdx}, rdy = {rdy}")
+            result.append(average)
+        return result
+
     def on_subscribe_image(self, msg):
         bridge = CvBridge()
         img = bridge.compressed_imgmsg_to_cv2(msg)
@@ -87,6 +108,11 @@ class FaceIdentifierNode(Node):
 
     def on_subscribe_region(self, msg):
         self.region_list_.append(msg)
+
+    def on_subscribe_thermo(self, msg):
+        bridge = CvBridge()
+        img = bridge.compressed_imgmsg_to_cv2(msg)
+        self.thermo_list_.append(img)
 
 
 def main(args=None):
