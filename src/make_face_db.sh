71dfcb53cc40a751e87d88dc032ea0bc54a641c4
Shintaro Sakoda <sakoda@keio.jp>
2023-03-15 10:12:02 +0000

    ファイルが存在しないときに止まってしまう問題を修正

--- src/make_face_db.sh
+++ src/make_face_db.sh
@@ -5,6 +5,6 @@ set -eux
 SCRIPT_DIR=$(dirname "$0")
 cd ${SCRIPT_DIR}
 
-rm ../assets/database/FACE_FEATURES.db 
+rm -f ../assets/database/FACE_FEATURES.db 
 python3 register_face_to_db.py
 python3 verify_db_data.py
